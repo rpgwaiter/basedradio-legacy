21f7285caab5c421a75603aff3488344aa3d98ef
Rpgwaiter <rpgwaiter@based.zone>
2019-09-16 17:34:50 -0500

    Initial Commit. WIP.

---
+++ js/config.js
@@ -0,0 +1,7 @@
+/* This is the BasedRadio Config File
+Fill this out and all your dreams will come true
+
+URL Prefix for the radio
+For example, if your site's URL is https://based.zone ,
+siteroot = "/radio" would setup basedradio at https://based.zone/radio */
+var siteroot = "/radio"
