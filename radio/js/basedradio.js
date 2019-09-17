42c511af94e623ab0c4ae544b09795a491af6027
Rpgwaiter <rpgwaiter@based.zone>
2019-09-17 03:28:04 -0500

    Javascript includes are lame.

--- radio/js/basedradio.js
+++ radio/js/basedradio.js
@@ -1,5 +1,27 @@
-// Load config file
-import { Config } from "./config.js";
+/***************************************************************************
+This is the BasedRadio Config
+Fill this out and all your dreams will come true */
+
+class Config {
+    constructor() {
+        /*URL Prefix for the radio
+               For example, if your site's URL is https://based.zone ,
+               siteroot = "/radio" would setup basedradio at https://based.zone/radio */
+        this.siteRoot = "/radio";
+        // Site Title
+        this.siteTitle = "BasedRadio - Video Game Music Stream";
+        // Directory where the radio files reside at
+        this.radioFiles = "/filehost/radiofiles";
+        // Opus Audio Source (Casted through Icecast or similar)
+        this.opusStream = "https://radio.based.zone/files/basedvgm.ogg";
+        // MP3 Audio Source (not as good as Opus)
+        this.mp3Stream = "https://radio.based.zone/files/basedvgm.mp3";
+        // Link to BasedRadio API
+        this.apiLink = "https://api.based.zone/status";
+    }
+}
+
+/****************************************************************************/
 
 siteConfig = new Config();
 
@@ -370,8 +392,7 @@ var Client = {
                 .replace(/no/, "")
             ) &&
             (($audio_element.type = 'audio/ogg; codecs="opus"'),
-                ($audio_element.src =
-                    Config.opusStream)),
+                ($audio_element.src = Config.opusStream)),
             $audio_element.load(),
                 ($audio_element.volume = e.volume / 100);
         });
