add6434b3cf061828d7dce697f9b0b9fbcf616e7
rpgwaiter <rpgwaiter@based.zone>
2021-05-15 14:25:18 -0500

    update flake

--- flake.nix
+++ flake.nix
@@ -1,24 +1,25 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    yarn2nix = { url = "github:nix-community/yarn2nix"; flake = false; };
+    nixpkgs.url = "github:nixos/nixpkgs";
   };
 
-  outputs = inputs:
-    let
-      api_url = "http://localhost:8000";
-      stream_url = "http://cast.based.radio/vgm.ogg";
-      pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
-      y2n = (import "${inputs.yarn2nix}/default.nix" { pkgs = pkgs; });
-    in with y2n;
-    {
-      packages.x86_64-linux.basedcast_web = pkgs.yarn2nix-moretea.mkYarnPackage {
-            name = "basedcast_web";
-            src = ./web;
-            packageJSON = ./web/package.json;
-            yarnLock = ./web/yarn.lock;
-            yarnNix = ./web/yarn.nix;
-      };
+  outputs = { self, nixpkgs }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    callPackage = pkgs.callPackage;
+  
+  in {
+    packages.x86_64-linux.basedradio = pkgs.yarn2nix-moretea.mkYarnPackage {
+      name = "basedradio";
+      src = ./.;
+      packageJSON = ./package.json;
+      yarnLock = ./yarn.lock;
+      yarnNix = ./yarn.nix;
     };
-      #mapAttrs' mkContainer outputs.packages
+
+    devShell.x86_64-linux =
+        pkgs.mkShell { buildInputs = [ 
+          #self.packages.x86_64-linux.basedradio 
+          pkgs.yarn 
+        ]; };
+  };
 }
