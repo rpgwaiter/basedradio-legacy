63b196823dcca1163835560fc22d690da5160cd8
rpgwaiter <rpgwaiter@based.zone>
2021-05-16 00:47:51 -0500

    but it is a derivation

--- flake.nix
+++ flake.nix
@@ -1,21 +1,32 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
   outputs = { self, nixpkgs }: let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
   in {
-    packages.x86_64-linux.basedradio = (pkgs.callPackage ./default.nix {});
+    packages.x86_64-linux.basedradio = pkgs.yarn2nix.mkYarnPackage {
+      name = "basedcradio";
+      src = ./.;
+      packageJSON = ./package.json;
+      yarnLock = ./yarn.lock;
+      yarnNix = ./yarn.nix;
+    };
 
     devShell.x86_64-linux = with pkgs;
-        mkShell { 
-          buildInputs = [ 
-            #self.packages.x86_64-linux.basedradio 
-            yarn
-            yarn2nix
-            nodePackages.node2nix
-          ]; 
-        };
+      mkShell { 
+        buildInputs = [ 
+          #self.packages.x86_64-linux.basedradio 
+          yarn
+          yarn2nix
+          nodePackages.node2nix
+          nodePackages.webpack-cli
+          nodePackages.webpack
+          nodePackages.npm
+          nodePackages.npm-check-updates
+        ]; 
+      };
   };
 }
